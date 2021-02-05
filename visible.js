59dc94dbae6bb6b22633d890b0538122dc49b9bc
nyoro-wrl <wrlrst@yahoo.co.jp>
2021-02-05 20:51:20 +0900

    どうでもいい処理をさせる

--- visible.js
+++ visible.js
@@ -1,3 +1,6 @@
 window.onload = function() {
+    // document.getElementById('url').textContent = location.pathname;
+    console.log(location.href + 'search?q=%s');
+    console.log(location.pathname + 'search?q=%s');
     document.body.classList.toggle('is-show');
 }
